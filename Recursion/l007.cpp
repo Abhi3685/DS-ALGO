3d3816296c4079f3228857a860b4b8cc3cbe0713
Abhishek Bindra <abhishek.bindra.ab@gmail.com>
2020-05-04 12:09:36 +0530

    Update

--- Recursion/l007.cpp
+++ Recursion/l007.cpp
@@ -24,6 +24,118 @@ bool wordBreak(string s, vector<string> &wordDict, unordered_map<string, bool> &
     return map[s] = res;
 }
 
+bool canPHword(vector<vector<char>> &box, int r, int c, string word)
+{
+    if (r >= box.size() || c + word.length() - 1 >= box[0].size())
+        return false;
+    for (int j = 0; j < word.length(); j++)
+    {
+        if (box[r][c + j] != '-' && box[r][c + j] != word[j])
+            return false;
+    }
+    return true;
+}
+
+bool canPVword(vector<vector<char>> &box, int r, int c, string word)
+{
+    if (r + word.length() - 1 >= box.size() || c >= box[0].size())
+        return false;
+    for (int j = 0; j < word.length(); j++)
+    {
+        if (box[r + j][c] != '-' && box[r + j][c] != word[j])
+            return false;
+    }
+    return true;
+}
+
+vector<bool> PHword(vector<vector<char>> &box, int r, int c, string word)
+{
+    vector<bool> loc(word.length());
+    for (int j = 0; j < word.length(); j++)
+    {
+        if (box[r][c + j] == '-')
+        {
+            box[r][c + j] = word[j];
+            loc[j] = true;
+        }
+    }
+    return loc;
+}
+
+void UnPHword(vector<vector<char>> &box, int r, int c, vector<bool> &loc)
+{
+    for (int j = 0; j < loc.size(); j++)
+    {
+        if (loc[j])
+        {
+            box[r][c + j] = '-';
+        }
+    }
+}
+
+vector<bool> PVword(vector<vector<char>> &box, int r, int c, string word)
+{
+    vector<bool> loc(word.length());
+    for (int j = 0; j < word.length(); j++)
+    {
+        if (box[r + j][c] == '-')
+        {
+            box[r + j][c] = word[j];
+            loc[j] = true;
+        }
+    }
+    return loc;
+}
+
+void UnPVword(vector<vector<char>> &box, int r, int c, vector<bool> &loc)
+{
+    for (int j = 0; j < loc.size(); j++)
+    {
+        if (loc[j])
+        {
+            box[r + j][c] = '-';
+        }
+    }
+}
+
+void crossWord(vector<vector<char>> &box, vector<string> &words, int idx)
+{
+    if (idx == words.size())
+    {
+        for (int i = 0; i < box.size(); i++)
+        {
+            for (int j = 0; j < box[0].size(); j++)
+            {
+                cout << box[i][j];
+            }
+            cout << endl;
+        }
+        return;
+    }
+    string word = words[idx];
+    for (int i = 0; i < box.size(); i++)
+    {
+        for (int j = 0; j < box[0].size(); j++)
+        {
+            if (box[i][j] == '-' || box[i][j] == word[0])
+            {
+                if (canPHword(box, i, j, word))
+                {
+                    vector<bool> loc = PHword(box, i, j, word);
+                    crossWord(box, words, idx + 1);
+                    UnPHword(box, i, j, loc);
+                }
+                if (canPVword(box, i, j, word))
+                {
+                    vector<bool> loc = PVword(box, i, j, word);
+                    crossWord(box, words, idx + 1);
+                    UnPVword(box, i, j, loc);
+                }
+            }
+        }
+    }
+}
+
 int main()
 {
     // WordBreak
@@ -31,5 +143,21 @@ int main()
     string s = "leetcode";
     vector<string> wordDict = {"leet", "code"};
     cout << wordBreak(s, wordDict, map);
+
+    // CrossWord
+    vector<vector<char>> box = {{'+', '+', '+', '+', '+', '+', '+', '+', '+', '-'},
+                                {'-', '+', '+', '+', '+', '+', '+', '+', '+', '-'},
+                                {'-', '-', '-', '-', '-', '-', '-', '+', '+', '-'},
+                                {'-', '+', '+', '+', '+', '+', '+', '+', '+', '-'},
+                                {'-', '+', '+', '+', '+', '+', '+', '+', '+', '-'},
+                                {'-', '+', '+', '+', '+', '-', '-', '-', '-', '-'},
+                                {'-', '-', '-', '-', '-', '-', '+', '+', '+', '-'},
+                                {'-', '+', '+', '+', '+', '+', '+', '+', '+', '-'},
+                                {'+', '-', '-', '-', '-', '-', '-', '-', '-', '-'},
+                                {'+', '+', '+', '+', '+', '+', '+', '+', '+', '+'}};
+    string str = "";
+    vector<string> words = {"CHEMISTRY", "PHYSICS", "GEOGRAPHY", "HISTORY", "MATHS", "CIVICS"};
+    crossWord(box, words, 0);
+
     return 0;
 }
