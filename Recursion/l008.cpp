761265bb56b27026c248696f07ef17b5e41ce56f
Abhishek Bindra <abhishek.bindra.ab@gmail.com>
2020-05-05 22:58:34 +0530

    Update

--- Recursion/l008.cpp
+++ Recursion/l008.cpp
@@ -22,9 +22,33 @@ int numDecodings(string s, int idx)
     return count;
 }
 
+string ans;
+int min_ = (int)1e8;
+void tugOfWar(vector<int> &arr, int idx, int sum1, int sum2, string set1, string set2)
+{
+    if (idx == (int)arr.size())
+    {
+        if (abs(sum1 - sum2) <= min_)
+        {
+            min_ = abs(sum1 - sum2);
+            ans = "[" + set1.substr(0, set1.size() - 2) + "][" + set2.substr(0, set2.size() - 2) + "]";
+        }
+        return;
+    }
+    if (idx >= (int)arr.size())
+        return;
+    tugOfWar(arr, idx + 1, sum1 + arr[idx], sum2, set1 + to_string(arr[idx]) + ", ", set2);
+    tugOfWar(arr, idx + 1, sum1, sum2 + arr[idx], set1, set2 + to_string(arr[idx]) + ", ");
+}
+
 int main()
 {
     string s = "226";
     cout << numDecodings(s, 0);
+
+    vector<int> arr = {2, 3, 4, 5, 6, 7};
+    tugOfWar(arr, 1, arr[0], 0, to_string(arr[0]) + ", ", "");
+    cout << ans;
+
     return 0;
 }
