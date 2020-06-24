c74feefffa0ba2eda115440393da0de5e38d863e
Abhishek Bindra <abhishek.bindra.ab@gmail.com>
2020-06-24 20:34:45 +0530

    Update

--- Stack&Queue/leetcode.cpp
+++ Stack&Queue/leetcode.cpp
@@ -140,6 +140,123 @@ int leet84(vector<int> &heights)
     return maxArea;
 }
 
+// =============== Leetcode 735 ===============
+vector<int> leet735(vector<int> &asteroids)
+{
+    if (asteroids.size() == 0)
+        return vector<int>();
+
+    stack<int> st;
+    for (int ele : asteroids)
+    {
+        if (ele > 0)
+            st.push(ele);
+        else
+        {
+            while (!st.empty() && st.top() > 0 && st.top() < -ele)
+            {
+                st.pop();
+            }
+            if (!st.empty() && st.top() == -ele)
+                st.pop();
+            else if (st.empty() || st.top() < 0)
+                st.push(ele);
+        }
+    }
+
+    asteroids.clear();
+    while (!st.empty())
+    {
+        asteroids.push_back(st.top());
+        st.pop();
+    }
+    reverse(asteroids.begin(), asteroids.end());
+    return asteroids;
+}
+
+// =============== Leetcode 503 ===============
+vector<int> leet503(vector<int> &nums)
+{
+    vector<int> res(nums.size(), -1);
+    stack<int> s;
+
+    // First Traversal: Same as next greater
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        while (!s.empty() && nums[i] > nums[s.top()])
+        {
+            res[s.top()] = nums[i];
+            s.pop();
+        }
+        s.push(i);
+    }
+
+    // 2nd Traversal: Resolving elements left in Stack
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        while (!s.empty() && nums[i] > nums[s.top()])
+        {
+            res[s.top()] = nums[i];
+            s.pop();
+        }
+    }
+
+    return res;
+}
+
+// =============== Leetcode 42 ===============
+int leet42(vector<int> &height)
+{
+    int ans = 0, n = height.size();
+    if (n == 0)
+        return 0;
+
+    int left[n], right[n];
+
+    // Left greatest calculation
+    left[0] = height[0];
+    for (int i = 1; i < n; i++)
+    {
+        left[i] = max(left[i - 1], height[i]);
+    }
+
+    // Right greatest calculation
+    right[n - 1] = height[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        right[i] = max(right[i + 1], height[i]);
+    }
+
+    // water += min(lh, rh) - curr_height
+    for (int i = 0; i < n; i++)
+    {
+        int lh = left[i], rh = right[i];
+        int minh = min(lh, rh);
+        ans += minh - height[i];
+    }
+
+    return ans;
+}
+
+// =============== Leetcode 901 ===============
+class leet901
+{
+public:
+    stack<pair<int, int>> st;
+
+    int next(int price)
+    {
+        int val = 1;
+        while (!st.empty() && st.top().first <= price)
+        {
+            val += st.top().second;
+            st.pop();
+        }
+        st.push({price, val});
+        return val;
+    }
+};
+
 int main()
 {
     // cout << leet20("[]{}()") << endl;
@@ -150,6 +267,31 @@ int main()
 
     // vector<int> heights = {2, 1, 5, 6, 2, 3};
     // cout << leet84(heights) << endl;
+
+    // vector<int> asteroids = {5, 10, -5};
+    // leet735(asteroids);
+    // for (int n : asteroids)
+    //     cout << n << " ";
+    // cout << endl;
+
+    // vector<int> nums = {1, 2, 1};
+    // vector<int> ans = leet503(nums);
+    // for (int n : ans)
+    //     cout << n << " ";
+    // cout << endl;
+
+    // vector<int> height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    // cout << leet42(height) << endl;
+
+    // leet901 S;
+    // cout << S.next(100) << " ";
+    // cout << S.next(80) << " ";
+    // cout << S.next(60) << " ";
+    // cout << S.next(70) << " ";
+    // cout << S.next(60) << " ";
+    // cout << S.next(75) << " ";
+    // cout << S.next(85) << " ";
+    // cout << endl;
 
     return 0;
 }
